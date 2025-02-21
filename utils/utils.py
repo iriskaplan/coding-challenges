41868ac138dcf9f29c5b845f35bfcd26a7e94f06
irisbur <irisburmi@gmail.com>
2025-02-21 13:15:07 +0200

    utils: add directions array for 2d arrays.

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,12 @@
 def is_index_valid(i, j, n, m):
     return 0 <= i < n and 0 <= j < m
 
+def get_4_dirs():
+    return [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+def get_8_dirs():
+    return [[1, 0], [-1, 0], [0, 1], [0, -1],
+            [-1, 1], [-1, -1], [1, -1], [1, 1],]
 
 def parse_grid():
     with (open('input.txt') as f):
