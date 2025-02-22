82081f740e1c9c6c521ad83ae14a18c005493d2d
irisbur <irisburmi@gmail.com>
2025-02-22 22:35:53 +0200

    AoC 2015: solve day 20.

--- Advent-of-code/2015/day 20.py
+++ Advent-of-code/2015/day 20.py
@@ -5,20 +5,23 @@ import math
 def main():
     n = 3
     gifts = 0
-    gifts_count = 120
+    gifts_count = 200
+    presents_to_house = 11
     while gifts < gifts_count:
         gifts = 0
         i = 1
         while i <= math.sqrt(n):
             if n % i == 0:
-                if n / i == i:
-                    gifts += i * 10
+                if n / i == i and n <= 50 * i:
+                    gifts += i * presents_to_house
                 else:
-                    gifts += (i * 10) + (n/i)*10
+                    if n <= 50 * i:
+                        gifts += (i * presents_to_house)
+                    if n <= 50 * int((n/i)):
+                        gifts += (n/i)*presents_to_house
             i += 1
         n += 1
     print(n-1)
-
 
 
 if __name__ == '__main__':
