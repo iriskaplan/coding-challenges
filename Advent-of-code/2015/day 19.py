6300dc3270a373e6203645d77260d6ce963aae1e
irisbur <irisburmi@gmail.com>
2025-02-22 18:36:29 +0200

    AoC 2015: solved part 2 but solution is a bit slow.

--- Advent-of-code/2015/day 19.py
+++ Advent-of-code/2015/day 19.py
@@ -7,29 +7,58 @@ def parse_input():
         for line in lines:
             line_list = line.split()
             if "=>" in line_list:
-                if line_list[0] not in replace_map:
-                    replace_map[line_list[0]] = [line_list[2]]
-                else:
-                    replace_map[line_list[0]].append(line_list[2])
+                replace_map[line_list[2]] = line_list[0]
             elif line:
                 s = line
         return s, replace_map
 
+def dfs(start_s, replace_map):
+    strings_q = [(start_s, 0)]
+    visited = {start_s:0}
+    while strings_q:
+        s, s_steps = strings_q.pop()
+
+        if s == "e":
+            print(s_steps)
+        # adjacent strings are strings can get to in one replace
+        adj_strings = replace_one_step(replace_map, s)
+        for adj_s in adj_strings:
+            if adj_s in visited and s_steps + 1 < visited[adj_s]:
+                visited[adj_s] = s_steps + 1
+            if adj_s not in visited:
+                strings_q.append((adj_s, s_steps+1))
+                visited[adj_s] = s_steps + 1
+    return visited["e"]
+
+
 def main():
     start_s, replace_map = parse_input()
+    steps = dfs(start_s, replace_map)
+    print(steps)
 
+def replace_subset(replace_map, strings):
+    all_replaced = set()
+    for s in strings:
+        replaced_strings = replace_one_step(replace_map, s)
+        all_replaced = all_replaced.union(replaced_strings)
+    return all_replaced
+
+def replace_one_step(replace_map, start_s):
     replaced_strings = set()
-    for i in range(len(start_s)):
-        if start_s[i] in replace_map:
-            for rep_s in replace_map[start_s[i]]:
-                new_s = start_s[:i] + rep_s + start_s[i+1:]
-                replaced_strings.add(new_s)
-        if i < (len(start_s) - 1) and start_s[i:i+2] in replace_map:
-            for rep_s in replace_map[start_s[i:i+2]]:
-                new_s = start_s[:i] + rep_s + start_s[i + 2:]
-                replaced_strings.add(new_s)
-    print(len(replaced_strings))
+    for rule in replace_map:
+        start = 0
+        indices = []
+        while True:
+            start = start_s.find(rule, start)
+            if start == -1:
+                break
+            indices.append(start)
+            start += len(rule)
 
+        for i in indices:
+            new_s = start_s[:i] + replace_map[rule] + start_s[i + len(rule):]
+            replaced_strings.add(new_s)
+    return replaced_strings
 
 
 if __name__ == '__main__':
